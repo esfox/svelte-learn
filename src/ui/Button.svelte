fdfc33d910c1b9ca29a6da3a4bd9a5c338597251
esfox <osairis100@gmail.com>
2021-03-12 19:47:15 +0800

    Initial commit

---
+++ src/ui/Button.svelte
@@ -0,0 +1,15 @@
+<script>
+
+  export let onClick;
+
+  let className;
+  export { className as class };
+
+</script>
+
+<button
+  class={`bg-green white fw7 bn br-pill shadow-hover pa3 pointer ${className || ''}`}
+  on:click={onClick}
+>
+  <slot />
+</button>
---
+++ src/ui/Input.svelte
@@ -0,0 +1,16 @@
+<script>
+
+  let className;
+  export { className as class };
+
+  export let value;
+  export let placeholder;
+
+</script>
+
+<input
+  type="text"
+  class={`bg-near-white bn pa3 ${className || ''}`}
+  placeholder={placeholder}
+  bind:value={value}
+>
